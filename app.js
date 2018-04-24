1f3373d40c9e735168f2fc8f08f38c25d136930b
Matilda Carlson <a.matildacarlson@gmail.com>
2018-04-24 10:42:51 +0200

    klar

--- app.js
+++ app.js
@@ -19,3 +19,27 @@ document.addEventListener('init', function(event) {
     	};
  	}
 });
+
+document.addEventListener('prechange', function(event) {
+  document.querySelector('ons-toolbar .center')
+    .innerHTML = event.tabItem.getAttribute('label');
+});
+
+var showTemplateDialog = function() {
+  var dialog = document.getElementById('my-dialog');
+
+  if (dialog) {
+    dialog.show();
+  } else {
+    ons.createElement('dialog.html', { append: true })
+      .then(function(dialog) {
+        dialog.show();
+      });
+  }
+};
+
+var hideDialog = function(id) {
+  document
+    .getElementById(id)
+    .hide();
+};
